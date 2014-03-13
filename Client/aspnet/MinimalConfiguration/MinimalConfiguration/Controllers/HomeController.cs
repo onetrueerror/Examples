6fc6ee75c6e1db1224da32448cffc83a97bf63e9
jgauffin <jonas@gauffin.org>
2014-03-13 23:12:23 +0100

    added MVC4

---
+++ Client/aspnet/MinimalConfiguration/MinimalConfiguration/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MinimalConfiguration.Models;
+using OneTrueError.Reporting;
+
+namespace MinimalConfiguration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "OneTrueError demonstration.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Post()
+        {
+            // exception will be detected and caught.
+            // an error page is shown
+            var repos = new FakeRepository();
+            repos.Save();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult PostViewModel(PostModel model)
+        {
+            // works with try/catch too
+            try
+            {
+                var repos = new FakeRepository();
+                repos.Save();
+            }
+            catch (Exception exception)
+            {
+                // just attach the model.
+                OneTrue.Report(exception, model);
+
+                /* Can also do like this:
+                 
+                OneTrue.Report(exception, new { MyViewModel = model, AdditionalData = new { Id = 1, Date = DateTime.Now } });
+                
+                 */
+            }
+
+            return View("Submitted");
+        }
+    }
+}
---
+++ Client/aspnet/MinimalConfiguration/MinimalConfiguration/FakeRepos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace MinimalConfiguration
+{
+    public class FakeRepository
+    {
+        public void Save()
+        {
+            throw new DataException("Failed to save data");
+        }
+    }
+}
